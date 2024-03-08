7bd1215c998bab06aad7684bbe2312a77bd191e3
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-03-08 19:03:30 +0100

    Feat -Created filter functionality

--- src/components/molecules/Notfound/styled.ts
+++ src/components/molecules/Notfound/styled.ts
@@ -6,7 +6,7 @@ export const NotFoundContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 100%;
 `;
 
 export const NotFoundGif = styled.img`
