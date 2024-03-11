22b20331a044ca0d846e8e5d0ff740f9c2c62600
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-03-11 12:49:16 +0100

    Improvements - About page

--- src/components/templates/PokemonAbout/index.tsx
+++ src/components/templates/PokemonAbout/index.tsx
@@ -8,12 +8,12 @@ const PokemonAbout = () => {
   return (
     <PokemonAboutContainer>
       <PokemonAboutWrapper>
-        <Paragraph size={1}>
+        <Image src={'/images/pikachu.png'} alt={'Pikachu'} width={200} height={200} />
+        <Paragraph size={2}>
           Discover Kanto Pokémon and Their Types
           Welcome to our web application dedicated to all Pokémon enthusiasts! Dive into the vibrant world of Kanto Pokémon and explore their diverse types in this comprehensive guide.
           Explore the Kanto Region: Embark on a nostalgic journey through the iconic Kanto region, where the Pokémon adventure first began. From the lush forests of Viridian to the bustling streets of Celadon City, encounter a wide array of Pokémon species that call Kanto home.
         </Paragraph>
-        <Image src={'/images/pikachu.png'} alt={'Pikachu'} width={200} height={200} />
       </PokemonAboutWrapper>
       <Button $variant="filled" href="/">Explore</Button>
     </PokemonAboutContainer>
